260c6bb40e0c47489680bd967892ffeb775ed473
patjagielski <jagielskipat@gmail.com>
2020-11-07 14:33:43 +0100

    added reading of file contents

--- Assignment7/7b/app.js
+++ Assignment7/7b/app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
 fs.watch('data',{recursive: true},(eventType, fileName) => {
-  
   console.log(`Event: ${eventType}`);
   console.log(`File Name: ${fileName}`);
+  if(eventType === 'change' && fileName.includes('.')){
+    fs.readFile(`data/${fileName}`, {encoding: 'utf-8'}, (err, data) =>{
+      return console.log(data);
+    })
+  }
 });
-
-
